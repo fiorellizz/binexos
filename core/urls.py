39bc1a149c610f729be1f8e284a034c467b4c64a
fiorellizz <fiorelligui@gmail.com>
2025-06-09 11:01:00 -0300

    fix: dados -> conferencia

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from core.interfaces.views import ConferenciaAPIView, DetalhadoProdutoAPIView, EstoqueFisicoAPIView, EstoquePorLocalAPIView
 
 urlpatterns = [
-    path('dados/', ConferenciaAPIView.as_view(), name='dados'),
+    path('dados/conferencia/', ConferenciaAPIView.as_view(), name='conferencia'),
     path("dados/detalhado_produto/", DetalhadoProdutoAPIView.as_view(), name="detalhado_produto"),
     path("dados/estoque_fisico/", EstoqueFisicoAPIView.as_view(), name="estoque_fisico"),
     path("dados/estoque_por_local/", EstoquePorLocalAPIView.as_view(), name="estoque_por_local"),
